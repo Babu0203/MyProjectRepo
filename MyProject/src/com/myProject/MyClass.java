7c8d422530fc0a544a6e579ae404e2be5ace4972
BABU REDDY K V <BABU REDDY K V@DESKTOP-MHVBRTF>
2020-04-26 17:45:45 +0530

    commited file

---
+++ MyProject/src/com/myProject/MyClass.java
@@ -0,0 +1,10 @@
+package com.myProject;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Project testing");
+	}
+
+}
